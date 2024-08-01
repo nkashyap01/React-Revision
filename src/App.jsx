83de26a0998ebcbde83287d360bcef4f1d299e2e
neha <nehakashyap1011@gmail.com>
2024-08-01 23:49:06 +0530

    started react revision

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <p className="font-bold bg-zinc-600">Hello</p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
