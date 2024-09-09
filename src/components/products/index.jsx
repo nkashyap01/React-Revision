da330f70e832dc04d056e5dd6ac25908667d98f6
neha <nehakashyap1011@gmail.com>
2024-09-09 23:19:36 +0530

    Apply the concept of list and keys

--- src/components/products/index.jsx
+++ src/components/products/index.jsx
@@ -1,10 +1,19 @@
 import ProductItem from "./product-items";
 
-// const productDummyData = ["product1","product2","product3"];
+const productDummyData = ["product1","product2","product3"];
 function ProductList() {
   return (
     <div>
       <h1>Ecommerce Project</h1>
+
+      <ul>
+        {
+          productDummyData.map((items, index)=>(
+            <li key={index}>{items}</li>
+            
+          ))
+        }
+      </ul>
       <ProductItem />
     </div>
   );
