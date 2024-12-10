280289b8cf43dfeed61a201984209cd04b384de7
Neha Kumari <nehakashyap1011@gmail.com>
2024-12-11 05:08:58 +0530

    Integrate useReducer to manage counter state

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,30 @@
-import { createContext, useState } from "react";
+// import { createContext, useState } from "react";
 import "./App.css";
 // import { CartProvider } from "./components/CartContext";
-import ChangeBg from "./components/ChangeBg";
-import Counter from "./components/Counter";
-import Fetch from "./components/Fetch";
-import Filter from "./components/Filter";
-import FunctionalComponent from "./components/functional-component";
-import ItemList from "./components/ItemList";
-import Login from "./components/Login";
-import ProductList from "./components/products";
-import RandomQuote from "./components/RandomQuote";
-import Routing from "./components/Routing";
-import SearchFilter from "./components/SearchFilter";
+// import ChangeBg from "./components/ChangeBg";
+import Count from "./components/Count";
+// import Counter from "./components/Counter";
+// import Fetch from "./components/Fetch";
+// import Filter from "./components/Filter";
+// import FunctionalComponent from "./components/functional-component";
+// import ItemList from "./components/ItemList";
+// import Login from "./components/Login";
+// import ProductList from "./components/products";
+// import RandomQuote from "./components/RandomQuote";
+// import Routing from "./components/Routing";
+// import SearchFilter from "./components/SearchFilter";
 // import ShoppingCart from "./components/ShoppingCart";
-import ShowInput from "./components/ShowInput";
-import Timer from "./components/Timer";
-import ToDo from "./components/ToDo";
-import Toggle from "./components/Toggle";
-import ChildA from "./hooks/ChildA";
+// import ShowInput from "./components/ShowInput";
+// import Timer from "./components/Timer";
+// import ToDo from "./components/ToDo";
+// import Toggle from "./components/Toggle";
+// import ChildA from "./hooks/ChildA";
 
 
-const ThemeContext = createContext();
+// const ThemeContext = createContext();
 function App() {
   // const items = ["item1", "item2", "item3", "item4", "item4"]
-  const [theme, setTheme] = useState('light');
+  // const [theme, setTheme] = useState('light');
 
   return (
     <>
@@ -34,21 +35,19 @@ function App() {
         {/* <Filter items={items}/> */}
         {/* <CartProvider> <ShoppingCart/></CartProvider> */}
 
-<ThemeContext.Provider value={{theme, setTheme}}>
+{/* <ThemeContext.Provider value={{theme, setTheme}}>
  <div className={`border-b-2 ${theme=='light'?'bg-gray-600':'bg-black'} h-[200px] w-[500px] flex items-center justify-center`}>
  <ChildA/>
  </div>
-</ThemeContext.Provider>
-      
+</ThemeContext.Provider> */}
+      <Count/>
       
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     
     </>
   );
 }
 
 export default App;
-export {ThemeContext};
+// export {ThemeContext};
 
