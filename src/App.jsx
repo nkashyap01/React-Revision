387a8db34a4a9a2b34bde406cd501ad9e2667c1b
Neha Kumari <nehakashyap1011@gmail.com>
2025-01-12 17:46:23 +0530

    Just testing github connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,20 @@
-// import { createContext, useState } from "react";
+
 import "./App.css";
-
-import Faq from "./practiceApps/accordian/Faq";
-
-// import { CartProvider } from "./components/CartContext";
-// import ChangeBg from "./components/ChangeBg";
-// import Fetch from "./components/Fetch";
-// import Filter from "./components/Filter";
-// import FunctionalComponent from "./components/functional-component";
-// import ItemList from "./components/ItemList";
-// import Login from "./components/Login";
-// import ProductList from "./components/products";
-// import RandomQuote from "./components/RandomQuote";
-// import Routing from "./components/Routing";
-// import SearchFilter from "./components/SearchFilter";
-// import ShoppingCart from "./components/ShoppingCart";
-// import ShowInput from "./components/ShowInput";
-// import Timer from "./components/Timer";
-// import ToDo from "./components/ToDo";
-// import Toggle from "./components/Toggle";
-// import ChildA from "./hooks/ChildA";
+import ToDo from "./practiceApps/todo/ToDo";
 
 
-// const ThemeContext = createContext();
+
+
+
 function App() {
-  // const items = ["item1", "item2", "item3", "item4", "item4"]
-  // const [theme, setTheme] = useState('light');
+ 
 
   return (
     <>
       <div>
         <p className="font-bold bg-zinc-600">Just Testing.........</p>
-        {/* <FunctionalComponent /> */}
-        {/* <ProductList /> */}
-        {/* <Filter items={items}/> */}
-        {/* <CartProvider> <ShoppingCart/></CartProvider> */}
-
-{/* <ThemeContext.Provider value={{theme, setTheme}}>
- <div className={`border-b-2 ${theme=='light'?'bg-gray-600':'bg-black'} h-[200px] w-[500px] flex items-center justify-center`}>
- <ChildA/>
- </div>
-</ThemeContext.Provider> */}
-      {/* <Accordian/> */}
-      {/* <StarRating noOfStars={5}/> */}
-  <Faq/>
+       <ToDo/>
+  
       </div>
      
     </>
@@ -51,5 +22,4 @@ function App() {
 }
 
 export default App;
-// export {ThemeContext};
 
---
+++ src/practiceApps/todo/ToDo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ToDo = () => {
+  return (
+    <div>
+      <p>To do App</p>
+    </div>
+  )
+}
+
+export default ToDo
