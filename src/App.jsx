a0c7b1ba25252f6b44003fd472e9d3a7859b6cae
Neha Kumari <nehakashyap1011@gmail.com>
2025-01-16 20:26:11 +0530

    Wrapped the concept of lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 
 import "./App.css";
 import FetchData from "./practiceApps/fetchData/FetchData";
+
 import ToDo from "./practiceApps/todo/ToDo";
+import React, {Suspense} from 'react'
 
-
+const LazyLoading = React.lazy(()=>import('./practiceApps/lazyLoading/LazyLoading'))
 
 
 
@@ -14,7 +16,9 @@ function App() {
     <>
       <div>
         <p className="font-bold bg-zinc-600">Just Testing.........</p>
-       <FetchData/>
+       <Suspense fallback={<div>Loading data......</div>}>
+        <LazyLoading/>
+        </Suspense>
   
       </div>
      
